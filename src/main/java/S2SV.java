8ac6172b5dc4f31393e874c8599bd342339ff468
parkysood <parkysood@gmail.com>
2025-05-23 11:30:17 -0700

    Main driver setup

--- src/main/java/S2SV.java
+++ src/main/java/S2SV.java
@@ -1,5 +1,12 @@
+import IR.SparrowParser;
+import IR.syntaxtree.Program;
+import IR.visitor.GJDepthFirst;
+
 public class S2SV {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws Exception {
+        new SparrowParser(System.in);
+
+        Program prog = SparrowParser.Program();
+        prog.accept(new GJDepthFirst<>(), null);
     }
 }
