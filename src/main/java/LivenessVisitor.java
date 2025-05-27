04ae8847c6d5ace5b7c34bd50ca15caca36b7629
parkysood <parkysood@gmail.com>
2025-05-26 21:30:31 -0700

    Created visit functions for all instructions in Sparrow function

--- src/main/java/LivenessVisitor.java
+++ src/main/java/LivenessVisitor.java
@@ -3,6 +3,24 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import IR.syntaxtree.Add;
+import IR.syntaxtree.Alloc;
+import IR.syntaxtree.Call;
+import IR.syntaxtree.FunctionDeclaration;
+import IR.syntaxtree.Goto;
+import IR.syntaxtree.Identifier;
+import IR.syntaxtree.IfGoto;
+import IR.syntaxtree.Instruction;
+import IR.syntaxtree.Label;
+import IR.syntaxtree.LessThan;
+import IR.syntaxtree.Load;
+import IR.syntaxtree.Move;
+import IR.syntaxtree.Multiply;
+import IR.syntaxtree.Print;
+import IR.syntaxtree.SetFuncName;
+import IR.syntaxtree.SetInteger;
+import IR.syntaxtree.Store;
+import IR.syntaxtree.Subtract;
 import IR.visitor.GJVoidDepthFirst;
 
 public class LivenessVisitor extends GJVoidDepthFirst<FunctionStruct> {
@@ -35,17 +53,17 @@ public class LivenessVisitor extends GJVoidDepthFirst<FunctionStruct> {
     // Helper functions
     // -----------------
 
-    private void addID(String fName, String varId, int lineNum) {
+    private void addID(String fName, String varId, int lineNumber) {
         // If varId is a function argument, no def or use needed
         if (aRegs.get(fName).containsKey(varId)) {
             if (!aIntervals.get(fName).containsKey(varId)) {
                 // First instance of varId
-                aIntervals.get(fName).put(varId, new Interval(lineNum, lineNum));
+                aIntervals.get(fName).put(varId, new Interval(lineNumber, lineNumber));
             } 
             else {
                 // Recalculate interval bounds
-                int first = Math.min(aIntervals.get(fName).get(varId).getFirst(), lineNum);
-                int last = Math.max(aIntervals.get(fName).get(varId).getLast(), lineNum);
+                int first = Math.min(aIntervals.get(fName).get(varId).getFirst(), lineNumber);
+                int last = Math.max(aIntervals.get(fName).get(varId).getLast(), lineNumber);
 
                 // Update interval for function -> varId
                 aIntervals.get(fName).put(varId, new Interval(first, last));
@@ -54,11 +72,11 @@ public class LivenessVisitor extends GJVoidDepthFirst<FunctionStruct> {
         }
         
         // varId is not a function argument, update def and use maps for function -> varId
-        addDef(fName, varId, lineNum);
-        addUse(fName, varId, lineNum);
+        addDef(fName, varId, lineNumber);
+        addUse(fName, varId, lineNumber);
     }
 
-    private void addDef(String fName, String varId, int lineNum) {
+    private void addDef(String fName, String varId, int lineNumber) {
         // First instance of function
         if (!defs.containsKey(fName)) {
             defs.put(fName, new HashMap<>());
@@ -66,15 +84,15 @@ public class LivenessVisitor extends GJVoidDepthFirst<FunctionStruct> {
 
         if (!defs.get(fName).containsKey(varId)) {
             // First instance of varId
-            defs.get(fName).put(varId, lineNum);
+            defs.get(fName).put(varId, lineNumber);
         } else {
             // Store earliest instance of varId
-            int first = Math.min(defs.get(fName).get(varId), lineNum);
+            int first = Math.min(defs.get(fName).get(varId), lineNumber);
             defs.get(fName).put(varId, first);
         }
     }
 
-    private void addUse(String fName, String varId, int lineNum) {
+    private void addUse(String fName, String varId, int lineNumber) {
         // First instance of function
         if (!uses.containsKey(fName)) {
             uses.put(fName, new HashMap<>());
@@ -82,27 +100,328 @@ public class LivenessVisitor extends GJVoidDepthFirst<FunctionStruct> {
 
         if (!uses.get(fName).containsKey(varId)) {
             // First instance of varId
-            uses.get(fName).put(varId, lineNum);
+            uses.get(fName).put(varId, lineNumber);
         } else {
             // Store latest instance of varId
-            int first = Math.max(uses.get(fName).get(varId), lineNum);
+            int first = Math.max(uses.get(fName).get(varId), lineNumber);
             uses.get(fName).put(varId, first);
         }
     }
 
-    private void addLabel(String fName, String label, int lineNum) {
+    private void addLabel(String fName, String label, int lineNumber) {
         // First instance of function
         if (!labels.containsKey(fName)) {
             labels.put(fName, new HashMap<>());
         }
 
         // Store line number of label
-        labels.get(fName).put(label, lineNum);
+        labels.get(fName).put(label, lineNumber);
     }
 
     // -----------------
     // Core functions
     // -----------------
 
-    
+    /**
+     * f0 -> "func"
+     * f1 -> FunctionName()
+     * f2 -> "("
+     * f3 -> ( Identifier() )*
+     * f4 -> ")"
+     * f5 -> Block()
+     */
+    @Override
+    public void visit(FunctionDeclaration n, FunctionStruct f) {
+        f.name = n.f1.f0.toString();
+        
+        // Clear loop intervals for this function
+        loopIntervals.clear();
+
+        // New variable -> interval map for this function
+        aIntervals.put(f.name, new HashMap<>());
+
+        // Process function arguments
+        if (n.f3.present()) {
+            for (int i = 0; i < n.f3.size(); i++) {
+                String paramName = ((Identifier) n.f3.elementAt(i)).f0.toString();
+                addID(f.name, paramName, 0);
+            }
+        }
+
+        // Process function block
+        n.f5.accept(this, f);
+        lineNum = 1;
+
+        // Liveness analysis
+    }
+
+    /**
+     * f0 -> LabelWithColon()
+     *       | SetInteger()
+     *       | SetFuncName()
+     *       | Add()
+     *       | Subtract()
+     *       | Multiply()
+     *       | LessThan()
+     *       | Load()
+     *       | Store()
+     *       | Move()
+     *       | Alloc()
+     *       | Print()
+     *       | ErrorMessage()
+     *       | Goto()
+     *       | IfGoto()
+     *       | Call()
+     */
+    @Override
+    public void visit(Instruction n, FunctionStruct f) {
+        f.lineNumber = lineNum++;
+        n.f0.accept(this, f);
+    }
+
+    /**
+     * f0 -> Identifier()
+     * f1 -> "="
+     * f2 -> IntegerLiteral()
+     */
+    @Override
+    public void visit(SetInteger n, FunctionStruct f) {
+        String id = n.f0.f0.toString();
+        addID(f.name, id, f.lineNumber);
+    }
+
+    /**
+     * f0 -> Identifier()
+     * f1 -> "="
+     * f2 -> "@"
+     * f3 -> FunctionName()
+     */
+    @Override
+    public void visit(SetFuncName n, FunctionStruct f) {
+        String id = n.f0.f0.toString();
+        addID(f.name, id, f.lineNumber);
+    }
+
+    /**
+     * f0 -> Identifier()
+     * f1 -> "="
+     * f2 -> Identifier()
+     * f3 -> "+"
+     * f4 -> Identifier()
+     */
+    @Override
+    public void visit(Add n, FunctionStruct f) {
+        String lhs = n.f0.f0.toString();
+        addID(f.name, lhs, f.lineNumber);
+
+        String arg1 = n.f2.f0.toString();
+        addID(f.name, arg1, f.lineNumber);
+
+        String arg2 = n.f4.f0.toString();
+        addID(f.name, arg2, f.lineNumber);
+    }
+
+    /**
+     * f0 -> Identifier()
+     * f1 -> "="
+     * f2 -> Identifier()
+     * f3 -> "-"
+     * f4 -> Identifier()
+     */
+    @Override
+    public void visit(Subtract n, FunctionStruct f) {
+        String lhs = n.f0.f0.toString();
+        addID(f.name, lhs, f.lineNumber);
+
+        String arg1 = n.f2.f0.toString();
+        addID(f.name, arg1, f.lineNumber);
+
+        String arg2 = n.f4.f0.toString();
+        addID(f.name, arg2, f.lineNumber);
+    }
+
+   /**
+    * f0 -> Identifier()
+    * f1 -> "="
+    * f2 -> Identifier()
+    * f3 -> "*"
+    * f4 -> Identifier()
+    */
+    @Override
+    public void visit(Multiply n, FunctionStruct f) {
+        String lhs = n.f0.f0.toString();
+        addID(f.name, lhs, f.lineNumber);
+
+        String arg1 = n.f2.f0.toString();
+        addID(f.name, arg1, f.lineNumber);
+
+        String arg2 = n.f4.f0.toString();
+        addID(f.name, arg2, f.lineNumber);
+    }
+
+    /**
+     * f0 -> Identifier()
+     * f1 -> "="
+     * f2 -> Identifier()
+     * f3 -> "<"
+     * f4 -> Identifier()
+     */
+    @Override
+    public void visit(LessThan n, FunctionStruct f) {
+        String lhs = n.f0.f0.toString();
+        addID(f.name, lhs, f.lineNumber);
+
+        String arg1 = n.f2.f0.toString();
+        addID(f.name, arg1, f.lineNumber);
+
+        String arg2 = n.f4.f0.toString();
+        addID(f.name, arg2, f.lineNumber);
+    }
+
+    /**
+     * f0 -> Identifier()
+     * f1 -> "="
+     * f2 -> "["
+     * f3 -> Identifier()
+     * f4 -> "+"
+     * f5 -> IntegerLiteral()
+     * f6 -> "]"
+     */
+    @Override
+    public void visit(Load n, FunctionStruct f) {
+        String lhs = n.f0.f0.toString();
+        addID(f.name, lhs, f.lineNumber);
+
+        String rhs = n.f3.f0.toString();
+        addID(f.name, rhs, f.lineNumber);
+    }
+
+    /**
+     * f0 -> "["
+     * f1 -> Identifier()
+     * f2 -> "+"
+     * f3 -> IntegerLiteral()
+     * f4 -> "]"
+     * f5 -> "="
+     * f6 -> Identifier()
+     */
+    @Override
+    public void visit(Store n, FunctionStruct f) {
+        String lhs = n.f1.f0.toString();
+        addID(f.name, lhs, f.lineNumber);
+
+        String rhs = n.f6.f0.toString();
+        addID(f.name, rhs, f.lineNumber);
+    }
+
+    /**
+     * f0 -> Identifier()
+     * f1 -> "="
+     * f2 -> Identifier()
+     */
+    @Override
+    public void visit(Move n, FunctionStruct f) {
+        String lhs = n.f0.f0.toString();
+        addID(f.name, lhs, f.lineNumber);
+
+        String rhs = n.f2.f0.toString();
+        addID(f.name, rhs, f.lineNumber);
+    }
+
+    /**
+     * f0 -> Identifier()
+     * f1 -> "="
+     * f2 -> "alloc"
+     * f3 -> "("
+     * f4 -> Identifier()
+     * f5 -> ")"
+     */
+    @Override
+    public void visit(Alloc n, FunctionStruct f) {
+        String lhs = n.f0.f0.toString();
+        addID(f.name, lhs, f.lineNumber);
+
+        String rhs = n.f4.f0.toString();
+        addID(f.name, rhs, f.lineNumber);
+    }
+
+    /**
+     * f0 -> "print"
+     * f1 -> "("
+     * f2 -> Identifier()
+     * f3 -> ")"
+     */
+    @Override
+    public void visit(Print n, FunctionStruct f) {
+        String id = n.f2.f0.toString();
+        addID(f.name, id, f.lineNumber);
+    }
+
+    /**
+     * f0 -> "goto"
+     * f1 -> Label()
+     */
+    @Override
+    public void visit(Goto n, FunctionStruct f) {
+        String label = n.f1.f0.toString();
+        
+        if (labels.containsKey(f.name) && labels.get(f.name).containsKey(label)) {
+            int destLineNum = labels.get(f.name).get(label);
+
+            if (destLineNum < f.lineNumber) {
+                loopIntervals.add(new Interval(destLineNum, f.lineNumber));
+            }
+        }
+    }
+
+    /**
+     * f0 -> "if0"
+     * f1 -> Identifier()
+     * f2 -> "goto"
+     * f3 -> Label()
+     */
+    @Override
+    public void visit(IfGoto n, FunctionStruct f) {
+        String condId = n.f1.f0.toString();
+        addID(f.name, condId, f.lineNumber);
+
+        String label = n.f3.f0.toString();
+        
+        if (labels.containsKey(f.name) && labels.get(f.name).containsKey(label)) {
+            int destLineNum = labels.get(f.name).get(label);
+
+            if (destLineNum < f.lineNumber) {
+                loopIntervals.add(new Interval(destLineNum, f.lineNumber));
+            }
+        }
+    }
+
+    /**
+     * f0 -> Identifier()
+     * f1 -> "="
+     * f2 -> "call"
+     * f3 -> Identifier()
+     * f4 -> "("
+     * f5 -> ( Identifier() )*
+     * f6 -> ")"
+     */
+    @Override
+    public void visit(Call n, FunctionStruct f) {
+        String resId = n.f0.f0.toString();
+        addID(f.name, resId, f.lineNumber);
+
+        String funcId = n.f3.f0.toString();
+        addID(f.name, funcId, f.lineNumber);
+
+        n.f5.accept(this, f);
+    }
+
+    /**
+     * f0 -> <IDENTIFIER>
+     */
+    @Override
+    public void visit(Label n, FunctionStruct f) {
+        String label = n.f0.toString();
+        addLabel(f.name, label, f.lineNumber);
+    }
 }
