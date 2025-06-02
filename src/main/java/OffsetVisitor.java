ec25ae3fa936f01458b04f681fddd40db8fb9336
parkysood <parkysood@gmail.com>
2025-06-02 13:47:25 -0700

    First pass for stack offset calculation

---
+++ src/main/java/OffsetVisitor.java
@@ -0,0 +1,89 @@
+
+import java.util.HashMap;
+import java.util.Map;
+
+import IR.token.Identifier;
+import sparrowv.Call;
+import sparrowv.FunctionDecl;
+import sparrowv.Move_Id_Reg;
+import sparrowv.Move_Reg_Id;
+import sparrowv.visitor.DepthFirst;
+
+public class OffsetVisitor extends DepthFirst{
+    Map<String, Map<String, Integer>> funcVarOffsets;       // function name -> identifier -> stack offset
+    String currentFunction;
+    int varOffset;
+
+    public OffsetVisitor() {
+        funcVarOffsets = new HashMap<>();
+    }
+
+    /*   Program parent;
+    *   FunctionName functionName;
+    *   List<Identifier> formalParameters;
+    *   Block block; */
+    @Override
+    public void visit(FunctionDecl n) {
+        
+        // Process function name
+        currentFunction = n.functionName.toString();
+        HashMap<String, Integer> varOffsets = new HashMap<>();
+
+        // Local variables start at offset -12
+        int offset = 0;
+        for (Identifier fp: n.formalParameters) {
+            varOffsets.put(fp.toString(), offset);
+            offset += 4;
+        }
+
+        // Store local variable offsets for current function
+        funcVarOffsets.put(currentFunction, varOffsets);
+
+        // Visit instructions, searching for local parameters
+        varOffset = -12;
+        n.block.accept(this);
+    }
+
+    /*   Identifier lhs;
+    *   Register rhs; */
+    @Override
+    public void visit(Move_Id_Reg n) {
+        String varName = n.lhs.toString();
+
+        Map<String, Integer> varOffsets = funcVarOffsets.get(currentFunction);
+        
+        // If this is first instance of a variable, give it a stack offset
+        if (!varOffsets.containsKey(varName)) {
+            varOffsets.put(varName, varOffset);
+            varOffset -= 4;
+        }
+    }
+
+    /*   Register lhs;
+    *   Identifier rhs; */
+    @Override
+    public void visit(Move_Reg_Id n) {
+        String varName = n.rhs.toString();
+
+        Map<String, Integer> varOffsets = funcVarOffsets.get(currentFunction);
+
+        // If this is first instance of a variable, give it a stack offset
+        if (!varOffsets.containsKey(varName)) {
+            varOffsets.put(varName, varOffset);
+            varOffset -= 4;
+        }
+    }
+
+    /*   Register lhs;
+    *   Register callee;
+    *   List<Identifier> args; */
+    @Override
+    public void visit(Call n) {
+        Map<String, Integer> varOffsets = funcVarOffsets.get(currentFunction);
+
+        for (Identifier a : n.args) {
+            varOffsets.put(a.toString(), varOffset);
+            varOffset -= 4;
+        }
+    }
+}
